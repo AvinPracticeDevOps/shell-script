3306f9b3b3804f2a458b140040524a57e84055d2
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 22:18:48 +0530

    calling 2nd script in 1st script

---
+++ 15-script-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NAME=Avinash
+
+echo "Before calling script-2 Name:$NAME"
+echo "PID of script-1: $$"
+
+#1st way
+./16-scirpt-2.sh
+
+echo "After calling script-2 Name: $NAME"
---
+++ 16-script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=AvinBandela
+
+echo "Value of script-2 Name: $NAME"
+echo "PID of scirpt-2 is: $$"
