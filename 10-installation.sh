762aaab303acff2f0a823a7f1cdbafb507f75b23
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 16:58:40 +0530

    checking super user,applying exit status

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    "you are not super user, please access with super user access priviliges"
+    echo "you are not super user, please access with super user access priviliges"
 else
-    "you are super user"
+    echo "you are super user"
 fi
 
 dnf install mysqll -y
