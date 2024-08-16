824790d85957cec8f24a0a6ec065cd769a8c0256
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 15:33:00 +0530

    without varibales

---
+++ 01-without-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Avinash: Hi Kumar, how are you?"
+echo "Kumar: I am fine Avinash, How about you!"
+echo "Avinash: I am good, How is your work?"
+echo "Kumar: Not bad, I want to change my domain to DevOps "
