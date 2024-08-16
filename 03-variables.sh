ba28bc277a22cec901709f3b4ac10825fe74bd05
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 15:38:35 +0530

    with variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Shakeel
+
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: I am fine $PERSON1, How about you!"
+echo "$PERSON1: I am good, How is your work?"
+echo "$PERSON2: Not bad, I want to change my domain to DevOps "
