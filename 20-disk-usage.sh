a1b2b7513ec315ab100674caf90c44d094f57f1b
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-17 00:35:27 +0530

    about disk usage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -9,7 +9,7 @@ MESSAGE=""
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+    FOLDER=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     
     if [ $USAGE -gt $THRESHOLD ]
     then
