3ca283482c372ee372c7b7b673652c9f16df4b21
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-17 00:28:37 +0530

    about disk usage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -12,7 +12,7 @@ do
     
     if [ $USAGE -gt $THRESHOLD ]
     then
-        echo "$FOLDER is more than $THRESHOLD, current usage is: $USAGE"
+        echo "$FOLDER is more than $THRESHOLD, current usage is: $USAGE \n"
     fi
 done <<< $DISK_USAGE
 
