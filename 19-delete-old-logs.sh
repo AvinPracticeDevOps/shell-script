494626ec893328d95542dc1b18da536d31608e66
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 23:51:15 +0530

    deleting old logs

--- 19-delete-old-logs.sh
+++ 19-delete-old-logs.sh
@@ -2,6 +2,17 @@
 
 SOURCE_DIR=/tmp/logs
 
+R="\e[31m"
+G="\e[32m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$G $SOURCE_DIR is exists $N"
+else
+    echo -e "$R $SOURCE_DIR is not there $N"
+    exit 1
+fi
+
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
 while IFS= read -r line
