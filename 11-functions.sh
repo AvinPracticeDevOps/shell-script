c8cee4b300c988b09b4072aa0437f8d56eaf7637
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 17:28:57 +0530

    checking super user,applying exit status

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "you are not super user, please access with super user"
+else
+    echo "you are super user"
+fi
+
+check_exit(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is FAILED"
+    else
+        echo "$2 is SUCCESS"
+    fi
+}
+
+dnf install git -y
+check_exit $? "Installing git"
+
+dnf install mysqll -y
+check_exit $? "Installing mysql"
+
+echo "Is script proceeding"
