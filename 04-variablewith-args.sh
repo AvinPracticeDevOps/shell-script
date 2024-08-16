cbe8f2e85575c77d44541154ff20406eb0658327
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 16:03:02 +0530

    passing args to variables

--- 04-variablewith-args.sh
+++ 04-variablewith-args.sh
@@ -4,6 +4,6 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1: Hi $PERSON2, how are you?"
-echo "$PERSON2: I am fine $PERSON1, How about you!"
+echo "${PERSON2}: I am fine $PERSON1, How about you!"
 echo "$PERSON1: I am good, How is your work?"
 echo "$PERSON2: Not bad, I want to change my domain to DevOps "
