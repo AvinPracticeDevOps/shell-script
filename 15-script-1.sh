50349405fb5f6f0a66d7cfc0ea6d5dca6cbb4cea
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-16 22:31:26 +0530

    calling 2nd script in 1st script

--- 15-script-1.sh
+++ 15-script-1.sh
@@ -6,10 +6,10 @@ echo "Before calling script-2 Name: $NAME"
 echo "PID of script-1: $$"
 echo "------------------------------------"
 #1st way
-#./16-script-2.sh
+./16-script-2.sh
 
 #2nd way
-source ./16-script-2.sh
+# source ./16-script-2.sh
 
 echo "------------------------------------"
 echo "After calling script-2 Name: $NAME"
