859c2c77f2c0801e16c312c725135152411f3aae
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-17 14:28:25 +0530

    checking disk usage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
+MESSAGE=""
 
 while IFS= read -r line
 do 
@@ -10,6 +11,8 @@ do
 
     if [ $USAGE -gt $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more usage than $DISK_THRESHOLD, current usage is: $USAGE"
+        MESSAGE="$FOLDER is more usage than $DISK_THRESHOLD, current usage is: $USAGE"
     fi
 done <<< $DISK_USAGE
+
+echo "$MESSAGE"
