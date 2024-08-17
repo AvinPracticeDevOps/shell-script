fb1dafd72c2281fc018229a095f6055cde6133bb
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-08-17 14:26:35 +0530

    checking disk usage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-THRESHOLD=6
-# MESSAGE=""
-
-# FOLDER=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+DISK_THRESHOLD=6
 
 while IFS= read -r line
-do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | grep xfs | awk -F " " '{print $NF}')
-    
-    if [ $USAGE -gt $THRESHOLD ]
-    then
-        MESSAGE+="$FOLDER is more than $THRESHOLD, current usage is: $USAGE \n"
-    fi
-    # echo -e "$MESSAGE"
-done <<< $DISK_USAGE
+do 
+    USAGE=$(echo "$line" | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo "$line" | awk -F " " '{print $NF}')
 
-echo -e "$MESSAGE"
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then
+        echo "$FOLDER is more usage than $DISK_THRESHOLD, current usage is: $USAGE"
+    fi
+done <<< $DISK_USAGE
